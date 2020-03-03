12f3366d06afa015169a05bf1bd4dbf9e2acbf75
Andrej Lehmann <alehmann@physnet.uni-hamburg.de>
2020-03-03 12:16:45 +0100

    cd = cd && ls

--- .bashrc_global.sh
+++ .bashrc_global.sh
@@ -24,3 +24,6 @@ export LC_ALL=en_US.UTF-8                #
 export LANG=en_US.UTF-8                  #
 export LANGUAGE=en_US.UTF-8              #
 # -----------------------------------------
+
+cd() { builtin cd "$@" && ls; }
+
